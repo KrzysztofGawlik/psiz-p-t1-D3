d44d42cb8133a2c310683c4ed61a3fcbc0b22b75
Krzysztof Gawlik <krzysztof.gaw@outlook.com>
2022-04-09 17:44:10 +0200

    Checked if arg counter is correct

--- task4_krz_gawlik/main.cpp
+++ task4_krz_gawlik/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main(void){
+int main(int argc, char** argv){
 
-    
+    if(argc != 2) {
+        cout << "You've provided wrong number of arguments (" << argc << ") - required: 2" << endl;
+    } else {
+        
+    }
 
+    cin.sync(); cin.get();
     return 0;
 }
