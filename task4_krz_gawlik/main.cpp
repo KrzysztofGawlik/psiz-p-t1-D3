ea690b9694fabf209f01bba16b61f171a01678a1
Krzysztof Gawlik <krzysztof.gaw@outlook.com>
2022-04-09 19:35:55 +0200

    Added createLog() function

--- task4_krz_gawlik/main.cpp
+++ task4_krz_gawlik/main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 using namespace std;
+
+void createLog(string message){
+    fstream logFile;
+    logFile.open("log.txt", ios::app);
+    time_t t = time(0);
+    string dt = ctime(&t);
+    logFile << endl << dt.substr(0, dt.length()-1) << " : " << message;
+    logFile.close();
+}
 
 int main(int argc, char** argv){
 
     if(argc != 3) {
-        cout << "You've provided wrong number of arguments (" << argc << ") - required: 2" << endl;
+        createLog("Provided wrong number of arguments (" + to_string(argc-1) + ") - required: 2.");
+        return 0;
     } else {
         fstream file_A, file_B;
         try {
             file_A.open(argv[1]);
             file_B.open(argv[2]);
         } catch (exception e) {
-            cout << "Could not open one of the files!" << endl;
+            createLog("Could not open at least one of provided files.");
             return 0;
         }
-        cout << "Files opened!\n1) " << argv[1] << "\n2) " << argv[2] << endl;
+        createLog((string) "Successfully opened two files: 1) " + argv[1] + " 2) " + argv[2]);
     }
 
     return 0;
