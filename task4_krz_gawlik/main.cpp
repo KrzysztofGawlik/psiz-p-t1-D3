500c4896c4ecb4cd2a12191369d786aa9fadf059
Krzysztof Gawlik <krzysztof.gaw@outlook.com>
2022-04-09 19:57:43 +0200

    Replaced try-catch with conditional statement, close files at the end

--- task4_krz_gawlik/main.cpp
+++ task4_krz_gawlik/main.cpp
@@ -17,17 +17,21 @@ int main(int argc, char** argv){
     if(argc != 3) {
         createLog("Provided wrong number of arguments (" + to_string(argc-1) + ") - required: 2.");
         return 0;
+    }
+
+    fstream file_A, file_B;
+    file_A.open(argv[1]);
+    file_B.open(argv[2]);
+    if(!file_A.is_open() || !file_B.is_open()){
+        createLog("Could not open at least one of provided files.");
+        return 0;
     } else {
-        fstream file_A, file_B;
-        try {
-            file_A.open(argv[1]);
-            file_B.open(argv[2]);
-        } catch (exception e) {
-            createLog("Could not open at least one of provided files.");
-            return 0;
-        }
         createLog((string) "Successfully opened two files: 1) " + argv[1] + " 2) " + argv[2]);
     }
+
+    file_A.close();
+    file_B.close();
+    createLog("Closed both files.");
 
     return 0;
 }
