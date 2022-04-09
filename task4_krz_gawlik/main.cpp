b787a54e008238f7f3c9dc6da6754e7c66febdd4
Krzysztof Gawlik <krzysztof.gaw@outlook.com>
2022-04-09 18:12:05 +0200

    Open a file and print sources

--- task4_krz_gawlik/main.cpp
+++ task4_krz_gawlik/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char** argv){
 
-    if(argc != 2) {
+    if(argc != 3) {
         cout << "You've provided wrong number of arguments (" << argc << ") - required: 2" << endl;
     } else {
-        
+        fstream file_A, file_B;
+        try {
+            file_A.open(argv[1]);
+            file_B.open(argv[2]);
+        } catch (exception e) {
+            cout << "Could not open one of the files!" << endl;
+            return 0;
+        }
+        cout << "Files opened!\n1) " << argv[1] << "\n2) " << argv[2] << endl;
     }
 
-    cin.sync(); cin.get();
     return 0;
 }
