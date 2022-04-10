6ad88f5f6bd35d872d84197e53838736c0060b96
Krzysztof Gawlik <krzysztof.gaw@outlook.com>
2022-04-10 16:44:32 +0200

    Added comments
    Former-commit-id: c9f5c0fc83f469a1be4488692a189a3cd1e536ef

--- task4_krz_gawlik/main.cpp
+++ task4_krz_gawlik/main.cpp
@@ -41,14 +41,16 @@ vector<vector<char>> convertToVector(fstream &file){
 }
 
 tuple<int, int, float, long> makeComparison(vector<vector<char>> seqA, vector<vector<char>> seqB){
+    
     createLog("Analyzing bits sequences... [clock start]");
     auto start = chrono::high_resolution_clock::now();
+
     vector<vector<char>> tmp;
     int diffs = 0, compared = 0;
     float ber = 0.;
     int size_A = seqA.size();
     int size_B = seqB.size();
-    // seqA always shorter or equal
+
     if(size_A > size_B){
         tmp = seqA;
         seqA = seqB;
@@ -56,6 +58,7 @@ tuple<int, int, float, long> makeComparison(vector<vector<char>> seqA, vector<ve
         size_A = seqA.size();
         size_B = seqB.size();
     }
+
     for(int i = 0; i < size_A; i++){
         for(int j = 0; j < 8; j++){
             if(seqA[i][j] != seqB[i][j]){
@@ -64,23 +67,28 @@ tuple<int, int, float, long> makeComparison(vector<vector<char>> seqA, vector<ve
             compared++;
         }
     }
+
     diffs += (size_B - size_A) * 8;
     ber = float(diffs) / float(size_B * 8.) * 100.;
     auto stop = chrono::high_resolution_clock::now();
     createLog("Analysis finished. [clock stop]");
     auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+
     tuple<int, int, float, long> results = make_tuple(compared, diffs, ber, duration.count());
+
     return results;
 }
 
 int main(int argc, char** argv){
     createLog("*** APP start");
 
+    // Check the number of args
     if(argc != 3) {
         createLog("Provided wrong number of arguments (" + to_string(argc-1) + ") - required: 2.");
         return 0;
     }
 
+    // Try to open both files
     fstream file_A, file_B;
     file_A.open(argv[1]);
     file_B.open(argv[2]);
@@ -94,7 +102,10 @@ int main(int argc, char** argv){
     vector<vector<char>> byteSeqA = convertToVector(file_A);
     vector<vector<char>> byteSeqB = convertToVector(file_B);
 
-    tuple<int, int, float, long> results = makeComparison(byteSeqA, byteSeqB);
+    // Pass both files to makeComparison function
+    tuple<int, int, float, long> results = makeComparison(file_A, file_B);
+
+    // Create message from given results
     string resultMsg = "*** RESULTS Compared bits: " + to_string(get<0>(results)) + 
                         "; Different bits: " + to_string(get<1>(results)) + 
                         "; BER: " + to_string(get<2>(results)) + "%" + 
@@ -102,6 +113,7 @@ int main(int argc, char** argv){
     createLog(resultMsg);
     cout << resultMsg << endl;
 
+    // Close files
     file_A.close();
     createLog((string) "File " + argv[1] + " closed.");
     file_B.close();
